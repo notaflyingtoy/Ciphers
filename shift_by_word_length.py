dd829b31b13ffbeaa6f3e8cfcfda95d467a2e7d1
notaflyingtoy <togher12345@outlook.com>
2020-12-15 17:19:15 +0000

    fix

--- shift_by_word_length.py
+++ shift_by_word_length.py
@@ -6,25 +6,26 @@
 
 #function to shift inputted strings by length of words forward if even and backwards if odd
 #use encrypt = false to decrypt (reverse)
-alpha="abcdefghijklmnopqrstuvwxyz"
-punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 
 
-# In[59]:
+# In[98]:
 
 
 def encrypter(text, encrypt=True):
+    alpha="abcdefghijklmnopqrstuvwxyz"
+    punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
     new_string = []
     split_by_space = text.split(" ")
     
     for words in split_by_space:
         new_letters = []
         word = list(words)
-        if not((len(word)%2) == 0 ^ encrypt == True):
+        if (len(word)%2 == 0):
             modifier = len(word)
         else:
             modifier = -len(word)
-        #print(modifier)
+        if encrypt!=True:
+            modifier=-modifier
         
         for char in word:
             #print(char)
@@ -48,5 +49,11 @@ def encrypter(text, encrypt=True):
 # In[ ]:
 
 
+encrypter("abcdef xyz", encrypt=False)
 
+
+# In[39]:
+
+
+6%2
 
